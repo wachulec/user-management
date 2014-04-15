471bf7e2df09f434ceba41fe26dc889485e1d613
Marcin Wachulec <wachulec.marcin@gmail.com>
2014-04-15 16:18:03 +0200

    Customers Controller list, details.
    oddzielenie tabeli klientów (jako partial phtml).
    rozdzielenie na akcje list i details.
    drobne rozbudowanie widoku akcji details.
    przeglądnięcie mechanizmu formularzy.

--- application/controllers/CustomersController.php
+++ application/controllers/CustomersController.php
@@ -14,18 +14,16 @@ class CustomersController extends Zend_Controller_Action
         $this->view->title="Klienci";
         $Customer=new Application_Model_DbTable_Customers();
         $this->view->customers=$Customer->fetchAll();
-        
-        //jeśli wybrano konkretnego klienta
-        $customer_id=$this->getRequest()->getParam('customers_id');
-        if(isset($customer_id) and !is_null($customer_id) ){
-            $s=$Customer->select()->where('customers_id=?',$customer_id);
-            $this->view->customer=$Customer->fetchRow($s);          
-        }
     }
 
     public function detailsAction()
     {
-        //
+        //jeśli wybrano konkretnego klienta
+        $this->view->title="Szczegóły";
+        $Customer=new Application_Model_DbTable_Customers();
+        $this->view->customers=$Customer->fetchAll();
+        $customer_id=$this->getRequest()->getParam('customers_id');
+        $this->view->customer=$this->view->customers->getRow($customer_id-1);
     }
 
 
